5e1b4fa5011de71f233195a102a00a45c98ac90d
João Victor Oliveira Couto <commits@jictyvoo.dev>
2023-09-02 21:12:38 -0300

    :ram: feat: Add a new call to newton-raphson method

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"math"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/jictyvoo/tec217_computing-methods/internal/methods"
@@ -129,6 +130,57 @@ func quest05Secant() {
 	reportResult("Secant", result, totalInteractions, &buffer)
 }
 
+func quest06PhysicsMadness() {
+	const (
+		x0      = 3.5
+		epsilon = 1e-8
+		N       = 1000
+	)
+
+	var (
+		buffer strings.Builder
+		method = methods.NewtonRaphsonMethod{}
+	)
+
+	fX := func(x float64) float64 { return math.Tan(0.1*x) - (9.2 * math.Pow(math.E, -x)) }
+	f1X := func(x float64) float64 {
+		return (9.2 * math.Pow(math.E, -x)) + (0.1 * math.Pow(1/math.Cos(0.1*x), 2))
+	}
+
+	result, totalInteractions, err := method.Run(
+		x0, fX, f1X, epsilon, N,
+	)
+
+	if err = errors.Join(err, utils.WriteInteractionAsCSV(&buffer, method.InteractionData())); err != nil {
+		reportError(err)
+		return
+	}
+	reportResult("PhysicsMadness(NewtonRaphson)", result, totalInteractions, &buffer)
+
+	{
+		const h = 1e-7 // Step size for finite difference
+
+		// Compute the estimate of the derivative at x = 3 using the finite difference
+		f3 := fX(3)
+		f3Prime := (fX(3+h) - f3) / h
+
+		// Compute the Derivative Estimate at x = 4 Using the Finite Difference
+		f4 := fX(4)
+		f4Prime := (fX(4+h) - f4) / h
+
+		slog.Info(
+			"Derivative in x = 3",
+			slog.String("estimate", strconv.FormatFloat(f3Prime, 'f', -1, 32)),
+			slog.String("result", strconv.FormatFloat(f1X(3), 'f', -1, 32)),
+		)
+		slog.Info(
+			"Derivative in x = 4",
+			slog.String("estimate", strconv.FormatFloat(f4Prime, 'f', -1, 32)),
+			slog.String("result", strconv.FormatFloat(f1X(4), 'f', -1, 32)),
+		)
+	}
+}
+
 func main() {
 	slog.SetDefault(
 		slog.New(
@@ -145,4 +197,5 @@ func main() {
 	quest03LinearIteration()
 	quest04NewtonRaphson()
 	quest05Secant()
+	quest06PhysicsMadness()
 }
